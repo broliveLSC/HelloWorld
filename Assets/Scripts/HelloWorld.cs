5d448b7502ed14f06ef6c0561fd776746b1bbc36
Brandon Olive <brandon.l.olive@lonestar.edu>
2025-01-17 11:37:59 -0600

    Changed hello to goodbye

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,7 +5,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("HELLO WORLD");
+        Debug.Log("Goodbye WORLD");
     }
 
     // Update is called once per frame
